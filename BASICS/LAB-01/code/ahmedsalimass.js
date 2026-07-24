4069b7c75d734c86bee5daf31d7f4a9b26d57be0
AASalim <ahmedsalim941682@gmail.com>
2026-07-24 13:52:50 +0100

    here is my assigment on personal information and use this ahmedsalim.js as my variable name

---
+++ BASICS/LAB-01/code/ahmedsalimass.js
@@ -0,0 +1,11 @@
+// my personal information
+
+const fullName = "Abdulrahman Salim Ahmed";
+let age = 23;
+let isStudent = true;
+let favoriteProgrmmingLanguage = "JavaScript";
+
+console.log(`My full name is ${fullName}`);
+console.log(`I am ${age} years old`);
+console.log(`Is a student: ${isStudent}`);
+console.log(`My favorite programming language is ${favoriteProgrmmingLanguage}`);
