14a508fcf7e3938163138009514019ba1e0a2d47
AASalim <ahmedsalim941682@gmail.com>
2026-07-24 14:29:46 +0100

    here is my assigment on arithmetic operation  and i used this calculator.js as my variable name

---
+++ BASICS/TICKET-01/code/calculator.js
@@ -0,0 +1,19 @@
+// my assignment on how a user can calculate two numbers and get the answer
+
+// the two variable i will be working with
+
+let num1 = 36;
+let num2 = 22;
+
+// here i will be carrying out operation on the two variable
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+// here i will be displaying the result 
+
+console.log(`Sum: ${sum}`);
+console.log(`Substract: ${difference}`);
+console.log(`Multiply: ${product}`);
+console.log(`Divide: ${quotient}`);
